ad8f1795daa991249d927287c224c03c484c42a0
Arsen <you@example.com>
2023-01-15 16:07:49 +0300

    first commit

---
+++ src/components/ProductsItem.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+  <div>
+    <a href="#" class="catalog__pic">
+      <img :src="imgsrc" :alt="title">
+    </a>
+    <div class="catalog__info">
+      <h3 class="catalog__title">
+        <a href="#">
+          {{ title }}
+        </a>
+      </h3>
+      <span class="catalog__price">
+        {{ price }}
+      </span>
+    </div>
+
+    <ul class="colors colors--black">
+      <li class="colors__item" v-for="clr in colors" :key="clr.id">
+        <label class="colors__label">
+          <input type="radio" class="colors__radio sr-only" :value="clr.code" v-model="color">
+          <span class="colors__value" :style="{'background-color': clr.code}"></span>
+        </label>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: '#73b6ea',
+    };
+  },
+
+  props: ['imgsrc', 'title', 'price', 'colors'],
+};
+</script>
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="catalog">
+  <ul class="catalog__list">
+    <li class="catalog__item" v-for="(product) in products" :key="product.id">
+      <ProductsItem
+          :imgsrc="product.imgsrc"
+          :title="product.title"
+          :price="product.price"
+          :colors="product.colors"
+        />
+    </li>
+  </ul>
+</section>
+</template>
+
+<script>
+
+import ProductsItem from './ProductsItem.vue';
+
+export default {
+  components: {
+    ProductsItem,
+  },
+  props: ['products'],
+};
+</script>
