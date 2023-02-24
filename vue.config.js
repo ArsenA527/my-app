7122e92628d013d20b7dc879b63b1ef3ca3692ca
Arsen <you@example.com>
2023-02-24 12:23:14 +0300

    make vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('@vue/cli-service');
+const {
+  defineConfig,
+} = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  publicPath: 'vue-app',
+};
